ddfa71d97ebd94da07a08aa6ce401c77b8173c44
Hirwa <hirwanike@gmail.com>
2025-09-04 08:31:29 +0200

    basic  api

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
 
 const app = express();
+require("dotenv").config()
+port = process.env.PORT
 
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-const port = process.env.PORT;
-const connection_string = process.env.CONNECTION_STRING;
 app.use(express.json());
-app.use("/api", signUpRoute);
+app.get("/Test", (req,res) => {
+ res.status(200).json("Tested successfully")
+ console.log(req.body)
+} )
+
+app.post("/cardTap", (req,res) => {
+ res.status(200).json("received successfully")
+ console.log(req.body)
+} )
 
 
-mongoose
-  .connect(connection_string)
-  .then(() => {
-    console.log("Successfully connected to the Database O");
-  })
-  .catch((error) => {
-    console.log("Failed to connect to the Database", error);
-    console.log(`your connection string is:`, connection_string);
-  });
-
-  server.listen(port, () => {
+  app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 });
